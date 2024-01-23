4128e4c3120bceedeb79b9a8f1025c514665d4a5
Admin <thakerniramay@gmail.com>
2024-01-23 18:37:28 +0530

    React

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -7,10 +7,9 @@ export default class App extends Component {
     }
 
     render() {
-        return <h1>Testing React Code</h1>;
+        return (<h1>Testing React Code</h1>);
     }
 }
 
-const appDiv = document.getElementById('add');
+const appDiv = document.getElementById("app");
 render(<App />, appDiv);
-
